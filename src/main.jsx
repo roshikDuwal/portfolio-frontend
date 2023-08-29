bde279695ae2f19ef24451f511d1b6fd23aef053
ROSHIK <105870336+roshikDuwal@users.noreply.github.com>
2023-08-29 17:10:02 +0545

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./index.css"
-import { Provider } from "react-redux"
-import { store } from './Store.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './Store.js';
 
-
-export const server="https://nodejs-portfolioapp.onrender.com"
+export const server = 'https://nodejs-portfolioapp.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,5 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </Provider>
   </React.StrictMode>
-
-)
+);
